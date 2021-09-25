f466b5fa7712515f67a05a396b621c8c8ef7c554
Jousephine <jousephinepardoo@gmail.com>
2021-09-25 11:10:49 -0300

    actualizando index y new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -14,7 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
